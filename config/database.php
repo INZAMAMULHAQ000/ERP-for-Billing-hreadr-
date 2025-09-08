a5410a1a1f221ab41c002ae9b70d4503511d8440
INZAMAMULHAQ000 <haq.cursor@gmail.com>
2025-09-08 15:26:57 +0400

    DB commit

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'supermarket_billing');
+define('DB_USERNAME', 'u443790490_Billing');
+define('DB_PASSWORD', 'AndroCoder2020');
+define('DB_NAME', 'u443790490_Billing');
 
 // Attempt to connect to MySQL database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
